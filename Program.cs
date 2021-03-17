cdd84d0bc21c99ed13bac420e342a0e632f0a82d
RETUR <nikitosix2002@gmail.com>
2021-03-17 08:41:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameByCash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
