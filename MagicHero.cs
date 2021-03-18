d19eeccbdb6d32c3aa59f5e0d2105b253790e4f9
RETUR <nikitosix2002@gmail.com>
2021-03-18 11:13:20 +0300

    MagicHero v0

--- MagicHero.cs
+++ MagicHero.cs
@@ -4,7 +4,42 @@ using System.Text;
 
 namespace GameByCash
 {
-    class MagicHero //: Hero
+    class MagicHero : Hero
     {
+        //мана
+        public uint MaxMana { get; set; } = 100;
+        private uint CurrentMana;
+        public uint CurMn
+        {
+            get
+            {
+                return CurrentMana;
+            }
+            set
+            {
+                CurrentMana = value;
+            }
+        }
+
+        //конструкторы
+        MagicHero(string n, Races r, Gender g, int age) : base(n, r, g, age)
+        {
+            CurrentMana = 100;
+        }
+        //заклинания ManaNeed это количество маны необходимое для каста с силой х1,
+        //а StrenthK коэфициент силы (количество маны на все заклинание ManaNeed * StrenthK)
+
+        //bool можно ли кастануть 
+        public bool CastCheck(int ManaNeed, int StrenthK)
+        {
+            if (ManaNeed * StrenthK > CurrentMana) return false;
+            return true;
+        }
+
+        //каст заклинания
+        public void MagicCast(int ManaNeed, int StrenthK)
+        {
+            CurrentMana = CurrentMana - Convert.ToUInt16(ManaNeed * StrenthK);
+        }
     }
 }
