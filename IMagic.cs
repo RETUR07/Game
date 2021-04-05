4f98f4c8ed0a68b3527c814559733b9463201cca
evgexabu@gmail.com <evgexabu@gmail.com>
2021-04-05 08:58:40 +0300

    Описал интерфейс IMagic. Добавил пустые методы в абстрактный класс MagicCast.

--- IMagic.cs
+++ IMagic.cs
@@ -6,5 +6,9 @@ namespace GameByCash
 {
     interface IMagic
     {
+        public bool MainCast(Hero TargetHero, int Strength);
+        public bool MainCast(Hero TargetHero);
+        public bool MainCast(int Strength);
+        public bool MainCast();
     }
 }
