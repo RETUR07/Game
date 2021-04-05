66d62d8ef5c1b0b70d75ec470d666452c3592a96
evgexabu@gmail.com <evgexabu@gmail.com>
2021-04-05 09:23:16 +0300

    Revert "Revert "Описал интерфейс IMagic. Добавил пустые методы в абстрактный класс MagicCast.""
    This reverts commit f5a19eb1caea21cfbd2572f256477e05c780cad6.

--- MagicCast.cs
+++ MagicCast.cs
@@ -6,5 +6,29 @@ namespace GameByCash
 {
     abstract class MagicCast:IMagic
     {
+        public uint MinMana;
+        public bool MoveAbility;
+        public bool TalkAbility;
+
+        public MagicCast(){
+
+        }
+
+        public bool MainCast(Hero TargetHero, int Strength)
+        {
+            return true;
+        }
+        public bool MainCast(Hero TargetHero)
+        {
+            return true;
+        }
+        public bool MainCast(int Strength)
+        {
+            return true;
+        }
+        public bool MainCast()
+        {
+            return true;
+        }
     }
 }
