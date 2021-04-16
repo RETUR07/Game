554e3616431522ed978ee12d313bf9792ef2e941
evgexabu@gmail.com <evgexabu@gmail.com>
2021-04-16 21:20:26 +0300

    -Написал заклинания(кроме брони) -Создал классы артефактов

--- AddHealth.cs
+++ AddHealth.cs
@@ -6,5 +6,53 @@ namespace GameByCash
 {
     class AddHealth : MagicCast
     {
+        public AddHealth(MagicHero spellCastingHero) 
+               : base(spellCastingHero, 2, true, false)//как выбирать компоненты t или f??
+        {
+            
+        }
+        public override bool MainCast(Hero targetHero, uint strength)//Проблемма оступа к полям (проверить) 
+        {
+            if(SpellCastingHero.CurMn >= MinMana 
+             && (SpellCastingHero.MoveCheck == MoveAbility || !MoveAbility)//Проверять ли? Выносить в класс Magic cast ли?
+             && (SpellCastingHero.SpeakCheck == SpeakAbility || !SpeakAbility))
+            {
+                if(targetHero.CurHlth + strength > targetHero.MaxHealth)
+                {
+                    targetHero.CurHlth = targetHero.MaxHealth;
+                } 
+                else{
+                    targetHero.CurHlth = targetHero.CurHlth + strength;                    
+                }
+                SpellCastingHero.CurMn -= MinMana;//Как менять ману хз 
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override bool MainCast(Hero targetHero)
+        {
+            if(SpellCastingHero.CurMn >= MinMana 
+             && (SpellCastingHero.MoveCheck == MoveAbility || !MoveAbility)
+             && (SpellCastingHero.SpeakCheck == SpeakAbility || !SpeakAbility))
+            {
+                if (targetHero.CurHlth + SpellCastingHero.CurMn / 2 > targetHero.MaxHealth)
+                {
+                    targetHero.CurHlth = targetHero.MaxHealth;                    
+                }
+                else
+                {
+                    targetHero.CurHlth = targetHero.CurHlth + SpellCastingHero.CurMn / 2;
+                }
+                SpellCastingHero.CurMn -= SpellCastingHero.CurMn / 2 * 2;//Может одна мана остаться
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
